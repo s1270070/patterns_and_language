d75c3c6feeea4d3c1e2c5739bfeb4d08fddf9830
s1270070 <s1270070@u-aizu.ac.jp>
2021-07-06 12:11:20 +0900

    [update] divide files

---
+++ orange-task/create.js
@@ -0,0 +1,42 @@
+/**
+ * get appropriate verb from values.
+ */
+function getVerb(v1, v2) {
+  if (v1 === v2) return 'remained flat';
+  else if (v1 < v2) return 'rose';
+  else if (v1 > v2) return 'fell';
+}
+
+/**
+ * get appropriate term from values.
+ */
+function getTerm(v1, v2, values) {
+  const startMonth = values[1];
+  const endMonth = values[3];
+  const startPrice = values[2];
+  const endPrice = values[4];
+
+  if (v1 === v2) return `at ${startPrice} from ${startMonth} to ${endMonth}`;
+  else return `from ${startPrice} in ${startMonth} by $diff to ${endPrice} in ${endMonth}`;
+}
+
+/**
+ * create appropriate sentence from 5 values
+ */
+function createSentence(values) {
+  const contentStrucuture = '$subject $verb $term.';
+  // get number from value.
+  const num1 = Number(values[2].match(/\d+/));
+  const num2 = Number(values[4].match(/\d+/));
+  const unit = values[2].match(/\D+/);
+
+  return contentStrucuture
+      // to upper case first character and replace.
+      .replace('$subject', values[0])
+      // get verb and replace.
+      .replace('$verb', getVerb(num1, num2))
+      // get term and replace.
+      .replace('$term', getTerm(num1, num2, values))
+      // create difference of 2 numbers and replace.
+      .replace('$diff', Math.abs(num2 - num1) + unit);
+}
